0f255635e46990957b43d568e6cead84bc226bb5
bogdankoval <40860432+bogdankoval@users.noreply.github.com>
2018-07-06 14:46:38 +0300

    Update lkcb_bot.py

--- lkcb_bot.py
+++ lkcb_bot.py
@@ -40,18 +40,28 @@ def main():
     new_offset = None
     today = now.day
     hour = now.hour
-
+'''
     while True:
+'''
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
 
         last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
+#        last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-        greet_bot.send_message(last_chat_id, 'Ебать-копать')
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+#        last_chat_name = last_update['message']['chat']['first_name']
+        greet_bot.send_message(last_chat_id, 'Бот для уведомления о поступлении новых сообщений в Личный кабинет ЦБ РФ')
+        greet_bot.send_message(last_chat_id, 'Это тестовая версия, на период отладки в целях изучения поведения бота она просто показывает количество непрочитанных сообщений в личном кабинете один раз, при запуске бота')
+  s = requests.Session()
+  r = s.post('https://portal4.cbr.ru/Account/Login', data = {'UserName':'leg10261021035762124', 'Password':'Hklm2070%'})
+  r = s.get('https://portal4.cbr.ru//Notification/GetNotificationCount')
+  greet_bot.send_message(last_chat_id, "У вас в личном кабинете " + r.text + " непрочитанных сообщений")
+        
+        
+        
+        
+        '''        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
             today += 1
 
@@ -61,7 +71,7 @@ def main():
 
         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
             greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
+'''            today += 1
 
         new_offset = last_update_id + 1
 if __name__ == '__main__':  
